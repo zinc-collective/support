3808bb56f0c605922a20177ba4b9334a932a64c1
Zee Spencer <50284+zspencer@users.noreply.github.com>
2020-05-03 11:33:44 -0700

    ✅ Validate presence and uniqueness of the team member attributes
    We're relying on thoughtbot's shoulda matchers, because they're a well maintained
    set of expectation handlers that allow us to fluently describ a bunch of
    the validations that are built into Rails.

--- hosted/spec/factories/team_members.rb
+++ hosted/spec/factories/team_members.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :team_member do
-    
+    public_schedule_slug { SecureRandom.uuid }
+    public_schedule_availability_email_address { "#{SecureRandom.uuid}@example.com" }
   end
 end
--- hosted/spec/models/team_member_spec.rb
+++ hosted/spec/models/team_member_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe TeamMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:team_member) { FactoryBot.build(:team_member) }
+  describe "#public_schedule_availability_email_address" do
+    it { is_expected.to validate_uniqueness_of(:public_schedule_availability_email_address) }
+    it { is_expected.to validate_presence_of(:public_schedule_availability_email_address) }
+  end
+
+  describe "#public_schedule_slug" do
+    it { is_expected.to validate_uniqueness_of(:public_schedule_slug) }
+    it { is_expected.to validate_presence_of(:public_schedule_slug) }
+  end
+
 end
