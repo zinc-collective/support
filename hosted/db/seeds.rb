edaade2675c6e892699583a4f097b92fae7b35bd
Zee Spencer <50284+zspencer@users.noreply.github.com>
2020-05-03 11:37:37 -0700

    🌱🔧  Seed a TeamMember when running db:seeds locally
    We like our seed files to give us a decent starting point for local development,
    as well as to be idempotent. This ensures that when we pick the project up
    in the future, we have a useful set of data to work with on our local machine
    without having to pull data from production.

--- hosted/db/seeds.rb
+++ hosted/db/seeds.rb
@@ -7,7 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 cohere_inbox = Inbox.find_or_create_by(name: "Cohere")
-cohere_inbox.update(confirmation_message: "Thanks! We'll get back with you!")
+cohere_inbox.update!(confirmation_message: "Thanks! We'll get back with you!")
 
 wegotyourback_inbox = Inbox.find_or_create_by(name: "WeGotYourBackToday")
-wegotyourback_inbox.update(confirmation_message: "Thanks! We'll get back with you!")
+wegotyourback_inbox.update!(confirmation_message: "Thanks! We'll get back with you!")
+
+team_member = TeamMember.find_or_create_by(public_schedule_slug: "zee")
+team_member.update!(public_schedule_availability_email_address: 'random-artichoke-bandwidth')
